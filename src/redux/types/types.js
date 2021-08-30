f1fb7fde818dae1dfec47fad2dd68e469212acf7
julio flores <julicesarflores12@gmail.com>
2021-08-30 10:27:12 -0400

    feat: error snackbar working

--- src/redux/types/types.js
+++ src/redux/types/types.js
@@ -3,7 +3,12 @@
  */
 
 export const types = {
+  // MESSAGE TYPES
   addMessage: '[Message] Add new message',
   clearMessage: '[Message] Clear a message',
   UpdateStopGeneration: '[Message] update stop generation',
+
+  // UI TYPES
+  setError: '[UI] add set error',
+  clearError: '[UI] add clear error',
 };
